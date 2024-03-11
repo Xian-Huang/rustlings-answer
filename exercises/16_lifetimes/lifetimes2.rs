f2a588e79439c94752b5b8f1ce80e6449b9124e6
lidingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-03-11 09:59:34 +0800

    lifetime

--- exercises/16_lifetimes/lifetimes2.rs
+++ exercises/16_lifetimes/lifetimes2.rs
@@ -8,7 +8,7 @@
 
 // I AM NOT DONE
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
     if x.len() > y.len() {
         x
     } else {
@@ -18,7 +18,7 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 fn main() {
     let string1 = String::from("long string is long");
-    let result;
+    let result: &str;
     {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
